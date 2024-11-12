9bf270f840b7ffda4e383f5eddeda9a7402db84d
Kinoe141 <molcanovamargarita321@gmail.com>
2024-11-12 18:29:53 +1000

    Добавлена структура дашборда

--- my_file.py
+++ my_file.py
@@ -1,1 +1,17 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
 
+bot = Bot(token='8118173051:AAFgVLf5hULwp8nWrrxi7KT4RJrw1-idZxM')
+dp = Dispatcher()
+
+@dp.message()
+async def cmd_start(message: Message):
+    await message.answer('Привет!')
+    await message.reply('Как дела?')
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
